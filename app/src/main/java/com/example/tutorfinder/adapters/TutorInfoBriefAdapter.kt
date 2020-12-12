f33f14a5aae8d0e8d6455c0e833cf2d77ff2dc5f
Abhishek Chouhan <abhich1607@gmail.com>
2020-12-12 17:09:00 +0530

    Bind all data to the ViewHolder

--- app/src/main/java/com/example/tutorfinder/adapters/TutorInfoBriefAdapter.kt
+++ app/src/main/java/com/example/tutorfinder/adapters/TutorInfoBriefAdapter.kt
@@ -8,22 +8,30 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.tutorfinder.R
 import com.example.tutorfinder.models.TutorInfoBrief
 
-class TutorInfoBriefAdapter(private val tutorInfoSet : Array<TutorInfoBrief>) :
-    RecyclerView.Adapter<TutorInfoBriefAdapter.TutorInfoBriefViewHolder>() {
+class TutorInfoBriefAdapter(private val tutorInfoSet: Array<TutorInfoBrief>) :
+        RecyclerView.Adapter<TutorInfoBriefAdapter.TutorInfoBriefViewHolder>() {
 
-    inner class TutorInfoBriefViewHolder(view : View) : RecyclerView.ViewHolder(view) {
-        val name : TextView = view.findViewById(R.id.name)
+    inner class TutorInfoBriefViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val name: TextView = view.findViewById(R.id.name)
+        val rating : TextView = view.findViewById(R.id.rating)
+        val subjects : TextView = view.findViewById(R.id.subjects)
+        val rate : TextView = view.findViewById(R.id.rate)
+        val profilePic : TextView = view.findViewById(R.id.profile_pic)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TutorInfoBriefViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.tutor_info_brief_card, parent, false)
+                .inflate(R.layout.tutor_info_brief_card, parent, false)
         return TutorInfoBriefViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: TutorInfoBriefViewHolder, position: Int) {
         holder.apply {
             name.text = tutorInfoSet[position].name
+            rating.text = tutorInfoSet[position].rating.toString()
+            subjects.text = tutorInfoSet[position].subjects[0]
+            rate.text = tutorInfoSet[position].rate.toString()
+            profilePic.text = tutorInfoSet[position].profilePic.toString()
         }
     }
 
