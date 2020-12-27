2b1d5e9f1129550e31fddda2dcd1563940260e0a
Abhishek Chouhan <abhich1607@gmail.com>
2020-12-27 18:00:27 +0530

    Tutor data submission possible

--- app/src/main/java/com/example/tutorfinder/TutorRegistrationActivity.kt
+++ app/src/main/java/com/example/tutorfinder/TutorRegistrationActivity.kt
@@ -1,14 +1,19 @@
 package com.example.tutorfinder
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
-class TutorRegistrationActivity: AppCompatActivity() {
+class TutorRegistrationActivity: AppCompatActivity(), View.OnClickListener {
 
     companion object {
         private val TAG = TutorRegistrationActivity::class.qualifiedName
@@ -17,9 +22,11 @@ class TutorRegistrationActivity: AppCompatActivity() {
     private lateinit var nameField: EditText
     private lateinit var rateField: EditText
     private lateinit var subjectsField: EditText
+    private lateinit var submitButton: Button
 
     private lateinit var auth: FirebaseAuth
     private var user: FirebaseUser? = null
+    private val db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +35,9 @@ class TutorRegistrationActivity: AppCompatActivity() {
         nameField = findViewById(R.id.name)
         rateField = findViewById(R.id.rate)
         subjectsField = findViewById(R.id.subjects)
+        submitButton = findViewById(R.id.submit_button)
+
+        submitButton.setOnClickListener(this)
 
         auth = Firebase.auth
     }
@@ -39,5 +49,31 @@ class TutorRegistrationActivity: AppCompatActivity() {
 
         //Set name field to what we get from Google account
         nameField.setText(user?.displayName)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id) {
+            R.id.submit_button -> submit()
+        }
+    }
+
+    private fun submit() {
+        val subjectsList = listOf<String>(subjectsField.text.toString())
+        val newUserInfo = hashMapOf(
+            "name" to nameField.text.toString(),
+            "rating" to 0,
+            "subjects" to subjectsList,
+            "rate" to rateField.text.toString().toInt(),
+        )
+
+        db.collection("tutors")
+            .document(user!!.uid)
+            .set(newUserInfo)
+            .addOnSuccessListener {
+                Log.d(TAG, "DocumentSnapshot added.")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
     }
 }
