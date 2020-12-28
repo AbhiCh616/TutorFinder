39f84fcdacbdec39cbb1559d063bc0eae6af22b0
Abhishek Chouhan <abhich1607@gmail.com>
2020-12-29 00:02:08 +0530

    Sign out buttom in student profile

--- app/src/main/java/com/example/tutorfinder/fragments/StudentProfileFragment.kt
+++ app/src/main/java/com/example/tutorfinder/fragments/StudentProfileFragment.kt
@@ -1,19 +1,88 @@
 package com.example.tutorfinder.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.tutorfinder.R
+import com.example.tutorfinder.activities.LoginActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class StudentProfileFragment: Fragment() {
+
+    companion object {
+        private val TAG = StudentProfileFragment::class.qualifiedName
+    }
+
+    // Google sign in
+    private var googleSignInClient: GoogleSignInClient? = null
+
+    // Firebase auth
+    private lateinit var auth: FirebaseAuth
+    private lateinit var currentUser: FirebaseUser
+
+    // Views
+    private lateinit var signOutButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
         return inflater.inflate(R.layout.student_profile_fragment, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Instantiate views
+        signOutButton = requireView().findViewById(R.id.sign_out_button)
+
+        // Set onClick listener
+        signOutButton.setOnClickListener { view -> signOut() }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // Get current user
+        currentUser = auth.currentUser!!
+    }
+
+    private fun signOut() {
+        // Firebase sign out
+        auth.signOut()
+
+        // Google sign out
+        googleSignInClient?.signOut()?.addOnCompleteListener(requireActivity()) {
+            val intent = Intent(requireActivity(), LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK or
+                    Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+        }
+    }
+
+
 }
