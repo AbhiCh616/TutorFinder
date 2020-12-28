cff7ada6ecb54bcc221fc420448ed139f92e4579
Abhishek Chouhan <abhich1607@gmail.com>
2020-12-29 00:35:34 +0530

    Chat fragments init

--- app/src/main/java/com/example/tutorfinder/fragments/StudentChatFragment.kt
+++ app/src/main/java/com/example/tutorfinder/fragments/StudentChatFragment.kt
@@ -7,12 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.tutorfinder.R
 
-class StudentChatFragment: Fragment() {
+class StudentChatFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.student_chat_fragment, container, false)
     }
