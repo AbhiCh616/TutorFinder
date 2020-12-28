e1cf91b371cef84ddb433ffea4b031065c410f49
Abhishek Chouhan <abhich1607@gmail.com>
2020-12-29 00:28:14 +0530

    Tutor activity completed

--- app/src/main/java/com/example/tutorfinder/activities/StudentActivity.kt
+++ app/src/main/java/com/example/tutorfinder/activities/StudentActivity.kt
@@ -1,6 +1,5 @@
 package com.example.tutorfinder.activities
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
@@ -8,14 +7,7 @@ import com.example.tutorfinder.R
 import com.example.tutorfinder.fragments.StudentChatFragment
 import com.example.tutorfinder.fragments.StudentProfileFragment
 import com.example.tutorfinder.fragments.StudentSearchFragment
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 class StudentActivity: AppCompatActivity() {
 
--- app/src/main/java/com/example/tutorfinder/activities/TutorActivity.kt
+++ app/src/main/java/com/example/tutorfinder/activities/TutorActivity.kt
@@ -1,13 +1,47 @@
 package com.example.tutorfinder.activities
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tutorfinder.R
+import com.example.tutorfinder.fragments.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class TutorActivity: AppCompatActivity() {
+
+    companion object {
+        private val TAG = TutorActivity::class.qualifiedName
+    }
+
+    // Declare views
+    private lateinit var bottomView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tutor_screen)
+
+        // Set views
+        bottomView = findViewById(R.id.bottom_navigation)
+        bottomView.setOnNavigationItemSelectedListener(privateListener)
+
+        // To show StudentSearchFragment when the activity starts
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, TutorChatFragment())
+                .commit()
     }
+
+    // To change fragments according to bottom navigation
+    private val privateListener =
+            BottomNavigationView.OnNavigationItemSelectedListener() { item: MenuItem ->
+                val fragment =
+                        when(item.itemId) {
+                            R.id.profile -> TutorProfileFragment()
+                            else -> TutorChatFragment()
+                        }
+
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
+                        .commit()
+
+                true
+            }
 }
