3e79d873f559deeb97545201907dc30ee26370fa
Abhishek Chouhan <abhich1607@gmail.com>
2021-01-17 00:29:57 +0530

    Logout button added in SelectRoleActivity

--- app/src/main/java/com/example/tutorfinder/activities/SelectRoleActivity.kt
+++ app/src/main/java/com/example/tutorfinder/activities/SelectRoleActivity.kt
@@ -8,7 +8,11 @@ import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tutorfinder.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.textview.MaterialTextView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
@@ -21,6 +25,9 @@ class SelectRoleActivity : AppCompatActivity(), View.OnClickListener {
         private val TAG = SelectRoleActivity::class.qualifiedName
     }
 
+    // Google sign in
+    private var googleSignInClient: GoogleSignInClient? = null
+
     // Firebase authentication
     private lateinit var auth: FirebaseAuth
     private lateinit var user: FirebaseUser
@@ -28,6 +35,8 @@ class SelectRoleActivity : AppCompatActivity(), View.OnClickListener {
     // Views
     private lateinit var selectStudentButton: MaterialButton
     private lateinit var selectTeacherButton: MaterialButton
+    private lateinit var name: MaterialTextView
+    private lateinit var signOutText: MaterialTextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,10 +48,22 @@ class SelectRoleActivity : AppCompatActivity(), View.OnClickListener {
         // Instantiate views
         selectStudentButton = findViewById(R.id.student)
         selectTeacherButton = findViewById(R.id.teacher)
+        name = findViewById(R.id.name)
+        signOutText = findViewById(R.id.sign_out_text)
 
         // set onClick listener
         selectStudentButton.setOnClickListener(this)
         selectTeacherButton.setOnClickListener(this)
+        signOutText.setOnClickListener(this)
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
     }
 
     override fun onStart() {
@@ -50,12 +71,16 @@ class SelectRoleActivity : AppCompatActivity(), View.OnClickListener {
 
         // Get logged in user
         user = auth.currentUser!!
+
+        // Display name of the user
+        name.text = (" " + user.displayName + "?")
     }
 
     override fun onClick(v: View) {
         when (v.id) {
             R.id.student -> triggerStudentRegistration()
             R.id.teacher -> triggerTutorRegistration()
+            R.id.sign_out_text -> signOut()
         }
     }
 
@@ -81,5 +106,20 @@ class SelectRoleActivity : AppCompatActivity(), View.OnClickListener {
         // Start tutor registration activity
         val tutorIntent = Intent(this, TutorRegistrationActivity::class.java)
         startActivity(tutorIntent)
+    }
+
+    private fun signOut() {
+        // Firebase sign out
+        auth.signOut()
+
+        // Google sign out
+        googleSignInClient?.signOut()?.addOnCompleteListener(this) {
+
+            // Start login activity and delete the track
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK or
+                    Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+        }
     }
 }
