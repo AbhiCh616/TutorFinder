775ac1cd1fecf9bf715d359728191140fa6df022
Abhishek Chouhan <abhich1607@gmail.com>
2021-01-21 19:09:33 +0530

    Tutor reg subject fill 1

--- app/src/main/java/com/example/tutorfinder/fragments/TutorRegSubject.kt
+++ app/src/main/java/com/example/tutorfinder/fragments/TutorRegSubject.kt
@@ -6,11 +6,33 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.tutorfinder.R
+import com.google.android.material.textview.MaterialTextView
+import java.util.*
 
 class TutorRegSubject: Fragment() {
 
+    // Views
+    private lateinit var currencyText: MaterialTextView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.tutor_reg_subject, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Instantiate views
+        currencyText = requireView().findViewById(R.id.currency_text)
+
+        // Set currency textview to appropriate symbol
+        currencyText.text = getCurrencySymbol()
+    }
+
+    // Return the currency of current locale
+    private fun getCurrencySymbol(): CharSequence? {
+        val defaultLocale = Locale.getDefault()
+        val currency = Currency.getInstance(defaultLocale)
+        return currency.symbol
     }
 
 }
