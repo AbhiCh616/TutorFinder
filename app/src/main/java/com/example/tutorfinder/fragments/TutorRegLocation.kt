d01561f6b187f6f95b8adfdfbf142c8a1b01221f
Abhishek Chouhan <abhich1607@gmail.com>
2021-01-22 00:15:51 +0530

    Slider animation and km value remain there on fragment change

--- app/src/main/java/com/example/tutorfinder/fragments/TutorRegLocation.kt
+++ app/src/main/java/com/example/tutorfinder/fragments/TutorRegLocation.kt
@@ -55,8 +55,7 @@ class TutorRegLocation : Fragment() {
                             in 71..80 -> R.drawable.distance_animation_8
                             in 81..90 -> R.drawable.distance_animation_9
                             else -> R.drawable.distance_animation_10
-                        }
-                )
+                        })
 
                 // Display the number of kms the slider is set to
                 distanceText.text = (String.format("%.1f", (progress / 10))
@@ -70,8 +69,33 @@ class TutorRegLocation : Fragment() {
             override fun onStartTrackingTouch(seekBar: CircularSeekBar?) {
 
             }
+        }) // Set onChange listener end
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // Set "street" image to the correct image
+        distanceAnimationImage.setImageResource(
+                when (slider.progress.toInt()) {
+                    0 -> R.drawable.nothing_box
+                    in 1..10 -> R.drawable.distance_animation_1
+                    in 11..20 -> R.drawable.distance_animation_2
+                    in 21..30 -> R.drawable.distance_animation_3
+                    in 31..40 -> R.drawable.distance_animation_4
+                    in 41..50 -> R.drawable.distance_animation_5
+                    in 51..60 -> R.drawable.distance_animation_6
+                    in 61..70 -> R.drawable.distance_animation_7
+                    in 71..80 -> R.drawable.distance_animation_8
+                    in 81..90 -> R.drawable.distance_animation_9
+                    else -> R.drawable.distance_animation_10
+                })
+
+        // Set distance text to match with slider
+        if(0 < slider.progress) {
+            distanceText.text = (String.format("%.1f", (slider.progress / 10))
+                    + " " + getString(R.string.km))
         }
-        )
     }
 
 }
