a57ef65f0d03201e0188bc8acc45d0614b3503da
Abhishek Chouhan <abhich1607@gmail.com>
2021-01-21 16:25:37 +0530

    Gender button click UI changes added

--- app/src/main/java/com/example/tutorfinder/fragments/TutorBasicInfoRegFragment.kt
+++ app/src/main/java/com/example/tutorfinder/fragments/TutorBasicInfoRegFragment.kt
@@ -4,13 +4,60 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.getColor
 import androidx.fragment.app.Fragment
 import com.example.tutorfinder.R
+import com.google.android.material.button.MaterialButton
 
-class TutorBasicInfoRegFragment : Fragment() {
+class TutorBasicInfoRegFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var genderMaleButton: MaterialButton
+    private lateinit var genderFemaleButton: MaterialButton
+    private lateinit var genderOtherButton: MaterialButton
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.basic_tutor_info_fragment, container, false)
+        return inflater.inflate(R.layout.tutor_reg_basic_info_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Instantiate views
+        genderMaleButton = requireActivity().findViewById(R.id.gender_male_button)
+        genderFemaleButton = requireActivity().findViewById(R.id.gender_female_button)
+        genderOtherButton = requireActivity().findViewById(R.id.gender_other_button)
+
+        // Set onClick listeners
+        genderMaleButton.setOnClickListener(this)
+        genderFemaleButton.setOnClickListener(this)
+        genderOtherButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.gender_male_button -> selectGenderMale()
+            R.id.gender_female_button -> selectGenderFemale()
+            R.id.gender_other_button -> selectGenderOther()
+        }
+    }
+
+    private fun selectGenderMale() {
+        // Set male button as selected and other gender buttons as unselected
+        genderMaleButton.setBackgroundColor(getColor(requireContext(), R.color.dark_blue))
+        genderFemaleButton.setBackgroundColor(getColor(requireContext(), R.color.grey_500))
+        genderOtherButton.setBackgroundColor(getColor(requireContext(), R.color.grey_500))
+    }
+
+    private fun selectGenderFemale() {
+        genderMaleButton.setBackgroundColor(getColor(requireContext(), R.color.grey_500))
+        genderFemaleButton.setBackgroundColor(getColor(requireContext(), R.color.dark_blue))
+        genderOtherButton.setBackgroundColor(getColor(requireContext(), R.color.grey_500))
+    }
+
+    private fun selectGenderOther() {
+        genderMaleButton.setBackgroundColor(getColor(requireContext(), R.color.grey_500))
+        genderFemaleButton.setBackgroundColor(getColor(requireContext(), R.color.grey_500))
+        genderOtherButton.setBackgroundColor(getColor(requireContext(), R.color.dark_blue))
     }
 
 }
