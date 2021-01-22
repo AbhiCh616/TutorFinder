534ec9922472fcae6f629b1c30c2bd5105f71e8e
Abhishek Chouhan <abhich1607@gmail.com>
2021-01-22 20:00:35 +0530

    Gender buttons in tutor reg retain selection UI

--- app/src/main/java/com/example/tutorfinder/fragments/TutorRegBasicInfo.kt
+++ app/src/main/java/com/example/tutorfinder/fragments/TutorRegBasicInfo.kt
@@ -11,9 +11,14 @@ import com.google.android.material.button.MaterialButton
 
 class TutorRegBasicInfo : Fragment(), View.OnClickListener {
 
+    // Views
     private lateinit var genderMaleButton: MaterialButton
     private lateinit var genderFemaleButton: MaterialButton
     private lateinit var genderOtherButton: MaterialButton
+
+    // To store user input
+    private enum class Gender {Male, Female, Other}
+    private var gender: Gender? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.tutor_reg_basic_info_fragment, container, false)
@@ -33,6 +38,16 @@ class TutorRegBasicInfo : Fragment(), View.OnClickListener {
         genderOtherButton.setOnClickListener(this)
     }
 
+    override fun onStart() {
+        super.onStart()
+
+        when(gender) {
+            Gender.Male -> selectGenderMale()
+            Gender.Female -> selectGenderFemale()
+            Gender.Other -> selectGenderOther()
+        }
+    }
+
     override fun onClick(v: View) {
         when (v.id) {
             R.id.gender_male_button -> selectGenderMale()
@@ -46,18 +61,26 @@ class TutorRegBasicInfo : Fragment(), View.OnClickListener {
         genderMaleButton.setBackgroundColor(getColor(requireContext(), R.color.dark_blue))
         genderFemaleButton.setBackgroundColor(getColor(requireContext(), R.color.grey_500))
         genderOtherButton.setBackgroundColor(getColor(requireContext(), R.color.grey_500))
+
+        gender = Gender.Male
     }
 
     private fun selectGenderFemale() {
+        // Set female button as selected and other gender buttons as unselected
         genderMaleButton.setBackgroundColor(getColor(requireContext(), R.color.grey_500))
         genderFemaleButton.setBackgroundColor(getColor(requireContext(), R.color.dark_blue))
         genderOtherButton.setBackgroundColor(getColor(requireContext(), R.color.grey_500))
+
+        gender = Gender.Female
     }
 
     private fun selectGenderOther() {
+        // Set "other" button as selected and other gender buttons as unselected
         genderMaleButton.setBackgroundColor(getColor(requireContext(), R.color.grey_500))
         genderFemaleButton.setBackgroundColor(getColor(requireContext(), R.color.grey_500))
         genderOtherButton.setBackgroundColor(getColor(requireContext(), R.color.dark_blue))
+
+        gender = Gender.Other
     }
 
 }
