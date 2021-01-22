c753c673e134f19f2cb9e6f040f3221e797c4a58
Abhishek Chouhan <abhich1607@gmail.com>
2021-01-22 20:48:12 +0530

    Ready to implement add new subject click

--- app/src/main/java/com/example/tutorfinder/fragments/TutorRegSubject.kt
+++ app/src/main/java/com/example/tutorfinder/fragments/TutorRegSubject.kt
@@ -1,24 +1,29 @@
 package com.example.tutorfinder.fragments
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.example.tutorfinder.R
 import com.google.android.flexbox.FlexboxLayout
 import com.google.android.material.textview.MaterialTextView
 import java.util.*
 
-class TutorRegSubject : Fragment() {
+class TutorRegSubject : Fragment(), View.OnClickListener {
 
     // Views
+    private lateinit var addSubjectButton: TextView
+    private lateinit var subjectFlexBox: FlexboxLayout
     private lateinit var currencyText: MaterialTextView
     private lateinit var perSpinner: Spinner
-    private lateinit var subjectFlexBox: FlexboxLayout
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.tutor_reg_subject, container, false)
@@ -28,9 +33,13 @@ class TutorRegSubject : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         // Instantiate views
+        addSubjectButton = requireView().findViewById(R.id.add_subject_text)
+        subjectFlexBox = requireView().findViewById(R.id.subjects_flex_box)
         currencyText = requireView().findViewById(R.id.currency_text)
         perSpinner = requireView().findViewById(R.id.per_spinner)
-        subjectFlexBox = requireView().findViewById(R.id.subjects_flex_box)
+
+        // Set onClick listener
+        addSubjectButton.setOnClickListener(this)
 
         // Set currency textview to appropriate symbol
         currencyText.text = getCurrencySymbol()
@@ -46,6 +55,16 @@ class TutorRegSubject : Fragment() {
             // Apply the adapter to the spinner
             perSpinner.adapter = adapter
         }
+
+    }
+
+    override fun onClick(v: View) {
+        when(v.id) {
+            R.id.add_subject_text -> addSubject()
+        }
+    }
+
+    private fun addSubject() {
 
     }
 
